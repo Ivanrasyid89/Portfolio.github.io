95b97ac4d52644fa26d47d93ce8af044f1cf19a0
Ivan Rasyid Ibnu Soetomo <98071016+Ivanrasyid89@users.noreply.github.com>
2025-02-27 11:51:07 +0700

    Update SALES ANALYSIS.sql

--- SQL/Bike Store/SALES ANALYSIS.sql
+++ SQL/Bike Store/SALES ANALYSIS.sql
@@ -1,33 +1,33 @@
 /* ANALISIS PENJUALAN */
 -- 1. Total Penjualan per tahun
 SELECT 
-	YEAR(order_date) AS year, 
+    YEAR(order_date) AS year, 
     MONTH(order_date) AS month, 
     SUM(quantity * (list_price - discount)) AS total_sales
 FROM 
-	orders o
+    orders o
 JOIN 
-	order_items oi ON o.order_id = oi.order_id
+    order_items oi ON o.order_id = oi.order_id
 GROUP BY 
-	YEAR(order_date), 
+    YEAR(order_date), 
     MONTH(order_date)
 ORDER BY 
-	year, 
+    year, 
     month;
     
 -- 2. Produk terlaris tiap tahun
 WITH top_product_per_year AS (
 	SELECT
-		YEAR(o.order_date) AS sales_year,
-        oi.product_id,
-        SUM(oi.quantity) AS total_sold
+	    YEAR(o.order_date) AS sales_year,
+            oi.product_id,
+            SUM(oi.quantity) AS total_sold
 	FROM
-		order_items oi
+	    order_items oi
 	JOIN 
-		orders o ON oi.order_id = o.order_id
+	    orders o ON oi.order_id = o.order_id
 	GROUP BY 
-		YEAR(o.order_date),
-        oi.product_id
+	    YEAR(o.order_date),
+            oi.product_id
 ),
 ranked_products AS (
     SELECT 
@@ -37,20 +37,20 @@ ranked_products AS (
         t.total_sold,
         RANK() OVER (PARTITION BY t.sales_year ORDER BY t.total_sold DESC) AS ranking
     FROM 
-		top_product_per_year t
+	top_product_per_year t
     JOIN 
-		products p ON t.product_id = p.product_id
+	products p ON t.product_id = p.product_id
 )
 SELECT 
     sales_year, 
     product_name, 
     total_sold
 FROM 
-	ranked_products
+    ranked_products
 WHERE 
-	rangking = 1
+    rangking = 1
 ORDER BY 
-	sales_year DESC;
+    sales_year DESC;
 
 -- 3. Produk terlaris tiap kategori
 WITH top_product_per_category AS (
@@ -59,13 +59,13 @@ WITH top_product_per_category AS (
         oi.product_id,
         SUM(oi.quantity) AS total_sold
     FROM 
-		order_items oi
+	order_items oi
     JOIN 
-		orders o ON oi.order_id = o.order_id
+	orders o ON oi.order_id = o.order_id
     JOIN 
-		products p ON oi.product_id = p.product_id
+	products p ON oi.product_id = p.product_id
     GROUP BY 
-		p.category_id, oi.product_id
+	p.category_id, oi.product_id
 ),
 ranked_products AS (
     SELECT 
@@ -76,11 +76,11 @@ ranked_products AS (
         t.total_sold,
         RANK() OVER (PARTITION BY t.category_id ORDER BY t.total_sold DESC) AS ranking
     FROM 
-		top_product_per_category t
+	top_product_per_category t
     JOIN 
-		products p ON t.product_id = p.product_id
+	products p ON t.product_id = p.product_id
     JOIN 
-		categories c ON t.category_id = c.category_id
+	categories c ON t.category_id = c.category_id
 )
 SELECT 
     category_name,
@@ -88,9 +88,9 @@ SELECT
     product_name, 
     total_sold
 FROM 
-	ranked_products
+    ranked_products
 WHERE 
-	ranking = 1;
+    ranking = 1;
 
 -- 4. Produk terlaris tiap brand
 WITH top_product_per_brand AS (
@@ -99,13 +99,13 @@ WITH top_product_per_brand AS (
         oi.product_id,
         SUM(oi.quantity) AS total_sold
     FROM 
-		order_items oi
+	order_items oi
     JOIN 
-		orders o ON oi.order_id = o.order_id
+	orders o ON oi.order_id = o.order_id
     JOIN 
-		products p ON oi.product_id = p.product_id
+	products p ON oi.product_id = p.product_id
     GROUP BY 
-		p.brand_id, 
+	p.brand_id, 
         oi.product_id
 ),
 ranked_products AS (
@@ -117,11 +117,11 @@ ranked_products AS (
         t.total_sold,
         RANK() OVER (PARTITION BY t.brand_id ORDER BY t.total_sold DESC) AS ranking
     FROM 
-		top_product_per_brand t
+	top_product_per_brand t
     JOIN 
-		products p ON t.product_id = p.product_id
+	products p ON t.product_id = p.product_id
     JOIN 
-		brands b ON t.brand_id = b.brand_id
+	brands b ON t.brand_id = b.brand_id
 )
 SELECT 
     brand_name,
@@ -129,6 +129,6 @@ SELECT
     product_name, 
     total_sold
 FROM 
-	ranked_products
+    ranked_products
 WHERE 
-	ranking = 1;
+    ranking = 1;

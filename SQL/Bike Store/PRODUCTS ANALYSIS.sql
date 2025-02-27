3053c668229f54c103b378d3faa1a1b4a7423d61
Ivan Rasyid Ibnu Soetomo <98071016+Ivanrasyid89@users.noreply.github.com>
2025-02-27 11:47:47 +0700

    Update PRODUCTS ANALYSIS.sql

--- SQL/Bike Store/PRODUCTS ANALYSIS.sql
+++ SQL/Bike Store/PRODUCTS ANALYSIS.sql
@@ -1,25 +1,26 @@
 /* ANALISIS PRODUK */
 -- 1. Produk Terlaris
 SELECT 
-	p.product_name, 
+    p.product_name, 
     SUM(oi.quantity) AS total_sold
 FROM 
-	order_items oi
+    order_items oi
 JOIN 
-	products p ON oi.product_id = p.product_id
+    products p ON oi.product_id = p.product_id
 GROUP BY 
-	p.product_name
+    p.product_name
 ORDER BY 
-	total_sold DESC
+    total_sold DESC
 LIMIT 10;
 
 -- 2. Rata-rata jumlah produk per transaksi
 SELECT AVG(total_quantity) AS avg_products_per_transaction
 FROM (
-    SELECT order_id, 
-    SUM(quantity) AS total_quantity
+    SELECT 
+	order_id, 
+        SUM(quantity) AS total_quantity
     FROM 
-		order_items
+	order_items
     GROUP BY 
-		order_id
+	order_id
 ) AS transaction_summary;
